fba89b33bdadd4fc9e75ee33ca1793f940657a00
erjigit7 <87127172+erjigit7@users.noreply.github.com>
2024-11-30 17:00:22 +0600

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import FullScreenIframe from "./FullScreenIframe";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/panorama-home" element={<HomePage />} />
+        <Route path="/panorama-3d-tour" element={<FullScreenIframe />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/FullScreenIframe.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import logo from './logo.png';
+
+const FullScreenIframe = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleIframeLoad = () => {
+    setIsLoaded(true); // Показываем логотип, когда iframe загружен
+  };
+
+  return (
+    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
+      {isLoaded && (
+        <a 
+          href="https://avangardstyle.kg/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          style={{
+            position: 'absolute',
+            top: '50px',
+            left: '50px',
+            zIndex: 1000,
+          }}
+        >
+          <img 
+            src={logo} 
+            alt="Company Logo" 
+            style={{ width: '150px', height: 'auto', cursor: 'pointer' }}
+          />
+        </a>
+      )}
+
+      {/* iframe, с обработчиком загрузки */}
+      <iframe
+        src="https://my.treedis.com/tour/panorama-park"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        allowFullScreen
+        onLoad={handleIframeLoad}
+      ></iframe>
+    </div>
+  );
+};
+
+export default FullScreenIframe;
