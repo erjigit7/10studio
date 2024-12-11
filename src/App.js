1c199e2b46600e5dd51f7d4952e64ff7e71c3e1b
erjigit7 <87127172+erjigit7@users.noreply.github.com>
2024-12-11 13:04:27 +0600

    фыв

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import FullScreenIframe from "./FullScreenIframe";
 import Home from "./Home";
+import HomeMoskva from "./HomeMoskva";
 import MoskvaSiti from "./MoskvaSiti";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
         <Route path="/panorama-home" element={<HomePage />} />
         <Route path="/panorama-3d-tour" element={<FullScreenIframe />} />
+        <Route path="/moskva-home" element={<HomeMoskva />} />
         <Route path="/moskva-sity-3d-tour" element={<MoskvaSiti />} />
       </Routes>
     </Router>
